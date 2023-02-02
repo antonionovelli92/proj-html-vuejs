6e78bf6bf1f254a79686a8b839813b97839d8b3b
antonionovelli92 <anto.novelli@hotmail.it>
2023-02-02 14:56:00 +0100

    add secton 'first'

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,9 +2,10 @@
 import SectionService from './_SectionService.vue';
 import SectionPlay from './_SectionPlay.vue';
 import SectionCourses from './_SectionCourses.vue';
+import SectionFirst from './_SectionFirst.vue';
 export default {
     name: 'AppMain',
-    components: { SectionService, SectionPlay, SectionCourses },
+    components: { SectionService, SectionPlay, SectionCourses, SectionFirst },
     data() {
         return
     }
@@ -17,6 +18,7 @@ export default {
         <section-service></section-service>
         <section-play></section-play>
         <section-courses></section-courses>
+        <section-first></section-first>
     </main>
 
 </template>
---
+++ src/components/_SectionFirst.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+    name: 'SectionFirst',
+}
+</script>
+
+<template>
+    <section id="firstStep">
+        <div class="content conteiner d-flex justify-content-center align-items-center">
+            <div class="content-text">
+                <h3>TAKE THE <span>FIRST STEP</span></h3>
+                <h6>The fascinating world of chess has its doors open for anyone</h6>
+            </div>
+            <button>bdhjkshbdv</button>
+
+        </div>
+
+    </section>
+</template>
+
+<style scoped lang="scss">
+@use '../assets/sass/partials/variables' as*;
+
+#firstStep {
+    background-image: url('../assets/img/mt-2236-home-bg2.jpg');
+    width: 100%;
+    min-height: 40px;
+    color: white;
+
+    .content {
+        margin: 70px auto;
+
+        h3 {
+            text-shadow: 1px 1px 5px black;
+            font-size: 60px;
+            margin-right: 4rem;
+
+            span {
+                color: $yellow;
+            }
+        }
+
+        h6 {
+            font-weight: lighter;
+        }
+
+    }
+
+}
+</style>
